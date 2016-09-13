33860f19cde046bfc31dd428516e69d531c18f62
m.kadayifci <m.kadayifci@IT-55>
2016-09-13 17:59:05 +0300

    ...

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AbraControlPanel
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.light1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.light2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.light1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.light2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 48);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(372, 246);
+            this.textBox1.TabIndex = 0;
+            // 
+            // light1
+            // 
+            this.light1.Location = new System.Drawing.Point(582, 133);
+            this.light1.Maximum = 100;
+            this.light1.Name = "light1";
+            this.light1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.light1.Size = new System.Drawing.Size(45, 161);
+            this.light1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(579, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sol Işık";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(642, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Sağ Işık";
+            // 
+            // light2
+            // 
+            this.light2.Location = new System.Drawing.Point(645, 133);
+            this.light2.Maximum = 100;
+            this.light2.Name = "light2";
+            this.light2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.light2.Size = new System.Drawing.Size(45, 161);
+            this.light2.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 350);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.light2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.light1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.light1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.light2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TrackBar light1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar light2;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Quobject.SocketIoClientDotNet.Client;
 
 namespace AbraControlPanel
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
+
+
+        public void SetSocket()
+        {
+
+            var socket = IO.Socket("http://192.168.100.104:8000");
+            socket.On(Socket.EVENT_CONNECT, () =>
+            {
+                socket.Emit("hi");
+
+            });
+
+            socket.On("hi", (data) =>
+            {
+
+                textBox1.Text += data + Environment.NewLine;
+                socket.Disconnect();
+            });
+
+        }
+
+
     }
 }
